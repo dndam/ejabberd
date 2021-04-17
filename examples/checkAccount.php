7ef3b4f6d9929f3eefcf0ab07608a421e0ac29d8
Dan <damian.dan@me.com>
2021-04-17 22:39:00 +0200

    Fixing issue nr 3: upgraded dependency on guzzlehttp

--- examples/checkAccount.php
+++ examples/checkAccount.php
@@ -5,6 +5,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Ejabberd\Rest\Client;
 
-$client = new Client(['apiUrl' => 'http://127.0.0.1:5443/api/']);
+$client = new Client([
+    'apiUrl' => 'http://127.0.0.1:5443/api/',
+    'host' => 'http://chat.example.com/'
+]);
 
 $client->checkAccount('admin@chat.example.com');
